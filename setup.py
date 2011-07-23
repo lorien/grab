078e77216fcb5ee6856a9be7015e370ab8ca83fc
Grigoriy Petukhov <lorien@lorien.name>
2011-07-23 23:52:22 +0700

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,16 @@
-import os
-from setuptools import setup
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-PACKAGE = 'grab'
-
-for dirpath, dirnames, filenames in os.walk(PACKAGE):
-    for i, dirname in enumerate(dirnames):
-        if dirname in ['.', '..']:
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[len(PACKAGE) + 1:] # Strip package directory + path separator
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
+from setuptools import setup, find_packages
 
 setup(
-    version = '0.2.13',
+    name = 'grab',
+    version = '0.2.15',
     description = 'Pycurl wrapper',
+    url = 'http://bitbucket.org/lorien/grab/',
     author = 'Grigoriy Petukhov',
     author_email = 'lorien@lorien.name',
-    url = 'http://bitbucket.org/lorien/grab/',
-    name = 'grab',
 
-    packages = packages,
-    package_data = {'grab': data_files},
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = ['lxml'],
 
     license = "BSD",
     keywords = "pycurl curl network parsing grabbing",
