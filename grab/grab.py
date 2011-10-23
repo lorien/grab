c32c5d81124b2c9e9d64a06e5928ae50c12500e6
Grigoriy Petukhov <lorien@lorien.name>
2011-10-23 10:41:03 +0600

    Change default accept-language header

--- grab/grab.py
+++ grab/grab.py
@@ -392,7 +392,7 @@ class Grab(object):
 
         return {
             'Accept': 'text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.%d' % randint(2, 5),
-            'Accept-Language': 'en-us;q=0.%d,en,ru;q=0.%d' % (randint(5, 9), randint(1, 4)),
+            'Accept-Language': 'en-us,en;q=0.%d' % (randint(5, 9)),
             'Accept-Charset': 'utf-8,windows-1251;q=0.7,*;q=0.%d' % randint(5, 7),
             'Keep-Alive': '300',
             'Expect': '',
