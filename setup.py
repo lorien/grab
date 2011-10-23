cfc774be977fe62482617628fe0e72e49192f1e5
Grigoriy Petukhov <lorien@lorien.name>
2011-10-23 13:17:48 +0600

    Change version to 0.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'grab',
-    version = '0.3.2',
+    version = '0.3.3',
     description = 'Pycurl wrapper',
     url = 'http://bitbucket.org/lorien/grab/',
     author = 'Grigoriy Petukhov',
