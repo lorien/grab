5ee8d9077e3477849bbb895e297b889af39711ff
Grigoriy Petukhov <lorien@lorien.name>
2011-10-30 13:31:46 +0600

    Change default imports in __init__.py

--- grab/__init__.py
+++ grab/__init__.py
@@ -1,5 +1,2 @@
-# TODO: rewrite * with explicit list of names to import
-from grab import *
-from bot import Bot, Request, Content
-from multi import multi_fetch
-from work import make_work
+from grab import (Grab, GrabError, DataNotFound, GrabNetworkError,
+                  GrabMisuseError, UploadContent, UploadFile)
