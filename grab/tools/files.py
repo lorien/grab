be9fa99c8f07e907aaf4e383b6170a965ade3f45
Grigoriy Petukhov <lorien@lorien.name>
2011-11-12 13:32:26 +0600

    Add unique_host function to tools.files

--- grab/tools/files.py
+++ grab/tools/files.py
@@ -2,6 +2,7 @@
 Miscelanius utilities which are helpful sometime.
 """
 import logging
+from urlparse import urlsplit
 
 def unique_file(path):
     """
@@ -17,6 +18,28 @@ def unique_file(path):
             count += 1
     logging.debug('Read %d lines from %s, unique: %d' % (count, path,
                                                          len(lines)))
+    with open(path, 'w') as out:
+        out.write(''.join(lines))
+    return len(lines)
+
+
+def unique_host(path):
+    """
+    Filter out urls with duplicated hostnames.
+    """
+
+    hosts = set()
+    lines = []
+    count = 0
+    with open(path) as inf:
+        for line in inf:
+            host = urlsplit(line).netloc
+            if not host in hosts:
+                lines.append(line)
+                hosts.add(host)
+            count += 1
+    logging.debug('Read %d lines from %s, unique hosts: %d' % (count, path,
+                                                               len(lines)))
     with open(path, 'w') as out:
         out.write(''.join(lines))
     return len(lines)
