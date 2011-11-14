3c2438049da743b53cb7a536ddc2db1a05302a33
Grigoriy Petukhov <lorien@lorien.name>
2011-11-14 17:33:50 +0600

    Add grab.tools.logs to configure logging module

---
+++ grab/tools/logs.py
@@ -0,0 +1,15 @@
+import logging
+
+def default_logging(grab_log='/tmp/grab.log'):
+    """
+    Customize logging output to display all log messages
+    except grab network logs.
+
+    Redirect grab network logs into file.
+    """
+
+    logging.basicConfig(level=logging.DEBUG)
+    glog = logging.getLogger('grab')
+    glog.propagate = False
+    hdl = logging.FileHandler(grab_log, 'w')
+    glog.addHandler(hdl)
