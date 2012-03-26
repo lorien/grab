4a245099c40fdfc00f6bfa6385db12554dcbb435
Grigoriy Petukhov <lorien@lorien.name>
2012-03-27 04:21:50 +0700

    Add smart_unicode method to tools.encoding module

--- grab/tools/encoding.py
+++ grab/tools/encoding.py
@@ -1,8 +1,18 @@
 def smart_str(value, encoding='utf-8'):
     """
-    Normalize unicode/btye string to byte string.
+    Normalize unicode/byte string to byte string.
     """
 
     if isinstance(value, unicode):
         value = value.encode(encoding)
+    return value
+
+
+def smart_unicode(value, encoding='utf-8'):
+    """
+    Normalize unicode/btye string to unicode string.
+    """
+
+    if not isinstance(value, unicode):
+        value = value.decode(encoding)
     return value
