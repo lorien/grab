fc8070dc367668d953aaa80d39afb507a3d075d7
Grigoriy Petukhov <lorien@lorien.name>
2012-04-27 17:36:52 +0700

    Add speed tests

---
+++ speed_grab.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+from grab import Grab
+from grab.tools.work import make_work
+from grab.tools.logs import default_logging
+import time
+import logging
+import urllib
+
+#logging.basicConfig(level=logging.DEBUG)
+
+def timer(func):
+    """
+    Display time taken to execute the decorated function.
+    """
+
+    def inner(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        total = time.time() - start
+        print 'Time: %.2f sec.' % total
+        return result
+    return inner
+
+
+@timer
+def main():
+    default_logging()
+    for x in xrange(500):
+        url = 'http://load.local/28k.html'
+        g = Grab()
+        g.go(url)
+        #g.css_text('title')
+        #urllib.urlopen(url).read()
+
+if __name__ == '__main__':
+    main()
---
+++ speed_spider.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+from grab.spider import Spider, Task
+from grab.tools.logs import default_logging
+import time
+import logging
+
+URL_28K = 'http://load.local/28k.html'
+
+def timer(func):
+    """
+    Display time taken to execute the decorated function.
+    """
+
+    def inner(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        total = time.time() - start
+        print 'Time: %.2f sec.' % total
+        return result
+    return inner
+
+
+class SpeedSpider(Spider):
+    def task_generator(self):
+        for x in xrange(500):
+            yield Task('load', url=URL_28K)
+
+    def task_load(self, grab, task):
+        pass
+
+
+@timer
+def main():
+    default_logging()
+    bot = SpeedSpider(thread_number=30)
+    bot.run()
+    print bot.render_stats()
+
+if __name__ == '__main__':
+    main()
