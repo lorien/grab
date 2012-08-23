b84bbee5fcd836be7c8dac725492d61bebba6eea
Grigoriy Petukhov <lorien@lorien.name>
2012-08-23 13:17:27 +0400

    Add formyip service

---
+++ grab/tools/service/formyip.py
@@ -0,0 +1,21 @@
+class ParsingError(Exception):
+    """
+    Raised when some unexpected HTML is found.
+    """
+
+
+def build_url():
+    return 'http://formyip.com/'
+
+
+def parse(g):
+    """
+    Parse HTML reponse from formyip.com website.
+    """
+
+    try:
+        ip = g.xpath('//strong').text.split('is ')[1]
+        country = g.xpath('//b[contains(text(), "Your Country")]').text.split(':')[1].strip()
+        return {'ip': ip, 'country': country}
+    except IndexError:
+        raise ParsingError('Could not parser formyip.com response')
