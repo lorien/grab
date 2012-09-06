faaf36b474999086ff96a8a470a7aa3ce7adc461
Grigoriy Petukhov <lorien@lorien.name>
2012-09-06 16:18:19 +0400

    Add base_dir option to tools.files.hashed_path. Add tools.files.clear_directory

--- grab/tools/files.py
+++ grab/tools/files.py
@@ -4,6 +4,8 @@ Miscelanius utilities which are helpful sometime.
 import logging
 from urlparse import urlsplit
 from hashlib import sha1
+import os
+import shutil
 
 def unique_file(path):
     """
@@ -46,16 +48,40 @@ def unique_host(path):
     return len(lines)
 
 
-def hashed_path(url, ext='jpg'):
+def hashed_path_details(url, ext='jpg', base_dir=None):
     _hash = sha1(url).hexdigest()
     a, b, tail = _hash[:2], _hash[2:4], _hash[4:]
+    directory = '%s/%s' % (a, b)
+    if base_dir is not None:
+        directory = '%s/%s' % (base_dir, directory)
     if ext is not None:
-        tail = '%s.%s' % (tail, ext)
-    rel_path = '%s/%s/%s' % (a, b, tail)
-    return rel_path
+        filename = '%s.%s' % (tail, ext)
+    full_path = '%s/%s' % (directory, filename)
+    return {'directory': directory,
+            'filename': filename,
+            'full_path': full_path,
+            }
+
+
+def hashed_path(url, ext='jpg'):
+    dtl = hashed_path_details(url, ext=ext)
+    return '%s/%s' % (dtl['directory'], dtl['filename'])
 
 
 # Alias for back-ward compatibility
 def hash_path(*args, **kwargs):
     logging.debug('This function name is depricated. Please use hashed_path function')
     return hashed_path(*args, **kwargs)
+
+
+def clear_directory(path):
+    """
+    Delete recursively all directories and files in
+    specified directory.
+    """
+
+    for root, dirs, files in os.walk(path):
+        for fname in files:
+            os.unlink(os.path.join(root, fname))
+        for _dir in dirs:
+            shutil.rmtree(os.path.join(root, _dir))
