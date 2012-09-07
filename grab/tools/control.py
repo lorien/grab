37ebcb707f985382957e9045be124d39c525fc90
Grigoriy Petukhov <lorien@lorien.name>
2012-09-07 13:27:07 +0400

    Add repeat function to grab.tools.control

--- grab/tools/control.py
+++ grab/tools/control.py
@@ -18,3 +18,26 @@ def sleep(lower_limit, upper_limit):
     sleep_time = randint(lower_limit_float, upper_limit_float) / 1000.0
     logger.debug('Sleeping for %f seconds' % sleep_time)
     time.sleep(sleep_time)
+
+
+def repeat(func, limit=3, args=None, kwargs=None, fatal_exceptions=()):
+    for x in xrange(limit):
+        ok = False
+        try:
+            if args is not None and kwargs is not None:
+                res = func(*args, **kwargs)
+            elif args is not None:
+                res = func(*args)
+            elif kwargs is not None:
+                res = func(**kwargs)
+            else:
+                res = func()
+            return res
+        except Exception, ex:
+            if isinstance(ex, fatal_exceptions):
+                raise
+            logging.error('', exc_info=ex)
+            if x == (limit - 1):
+                raise
+        else:
+            break
