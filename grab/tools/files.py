91387e2e7a29bac9e534424bffe24ed22c62b2b8
Grigoriy Petukhov <lorien@lorien.name>
2012-09-07 13:27:33 +0400

    Fix bug in hashed_path function

--- grab/tools/files.py
+++ grab/tools/files.py
@@ -56,6 +56,8 @@ def hashed_path_details(url, ext='jpg', base_dir=None):
         directory = '%s/%s' % (base_dir, directory)
     if ext is not None:
         filename = '%s.%s' % (tail, ext)
+    else:
+        filename = tail
     full_path = '%s/%s' % (directory, filename)
     return {'directory': directory,
             'filename': filename,
