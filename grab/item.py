b2f8cf20c4eade9b06d695edf4cf235ff7672254
Grigoriy Petukhov <lorien@lorien.name>
2013-02-24 05:05:45 +0400

    Improve grab.item

--- grab/item.py
+++ grab/item.py
@@ -25,9 +25,12 @@ Usage example:
 """
 from __future__ import absolute_import
 from abc import ABCMeta, abstractmethod
+from datetime import datetime
 
 from .tools.lxml_tools import get_node_text
+from .error import DataNotFound
 
+NONE = object()
 
 class Field(object):
     """
@@ -36,50 +39,134 @@ class Field(object):
 
     __metaclass__ = ABCMeta
 
-    def __init__(self, xpath_exp):
-        self.xpath_exp = xpath_exp
+    def __init__(self, xpath=None, default=NONE, empty_default=NONE):
+        self.xpath_exp = xpath
+        self.default = default
+        self.empty_default = empty_default
 
     @abstractmethod
-    def get(self, item): pass
+    def __get__(self, obj, objtype):
+        pass
+
+    def __set__(self, obj, value):
+        obj._cache[self.attr_name] = value
+
+
+def cached(func):
+    def internal(self, item, itemtype):
+        if self.attr_name in item._cache:
+            return item._cache[self.attr_name]
+        else:
+            value = func(self, item, itemtype)
+            item._cache[self.attr_name] = value
+            return value
+    return internal
+
+
+def default(func):
+    def internal(self, item, itemtype):
+        try:
+            value = func(self, item, itemtype)
+        except DataNotFound:
+            if self.default is not NONE:
+                value = self.default
+            else:
+                raise
+        else:
+            if self.empty_default is not NONE:
+                if not value:
+                    value = self.empty_default
+        item._cache[self.attr_name] = value
+        return value
+    return internal
+
+
+def func_field(func):
+    class FuncField(Field):
+        @cached
+        @default
+        def __get__(self, item, itemtype):
+            return func(self, item._tree)
+    #FuncField.__name__ = func.__name__
+    return FuncField()
 
 
 class IntegerField(Field):
-    def get(self, item):
-        return int(get_node_text(item.tree.xpath(self.xpath_exp)[0]))
+    @cached
+    @default
+    def __get__(self, item, itemtype):
+        if self.xpath_exp is None:
+            return None
+        value = get_node_text(item._tree.xpath(self.xpath_exp)[0])
+        if self.empty_default is not NONE:
+            if value == "":
+                return self.empty_default
+        return int(value)
 
 
 class StringField(Field):
-    def get(self, item):
-        return get_node_text(item.tree.xpath(self.xpath_exp)[0])
+    @cached
+    @default
+    def __get__(self, item, itemtype):
+        if self.xpath_exp is None:
+            return None
+        return get_node_text(item._tree.xpath(self.xpath_exp)[0])
 
 
 class DateTimeField(Field):
-    def __init__(self, xpath_exp, datetime_format):
+    def __init__(self, xpath, datetime_format, *args, **kwargs):
         self.datetime_format = datetime_format
-        super(DateTimeField, self).__init__(xpath_exp)
+        super(DateTimeField, self).__init__(xpath, *args, **kwargs)
 
-    def get(self, item):
-        from datetime import datetime
+    @cached
+    @default
+    def __get__(self, item, itemtype):
+        _str = get_node_text(item._tree.xpath(self.xpath_exp)[0])
+        return datetime.strptime(_str, self.datetime_format)
 
-        datetime_str = get_node_text(item.tree.xpath(self.xpath_exp)[0])
 
-        return datetime.strptime(datetime_str, self.datetime_format)
+class FuncField(Field):
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        super(FuncField, self).__init__(*args, **kwargs)
+
+    @cached
+    @default
+    def __get__(self, item, itemtype):
+        return self.func(self, item._tree)
 
 
 class ItemBuilder(type):
     def __new__(cls, name, base, namespace):
-        _fields = {}
+        field_list = []
         for attr in namespace:
             if isinstance(namespace[attr], Field):
                 field = namespace[attr]
-                _fields[attr] = field
-                namespace[attr] = property(field.get)
-
+                field.attr_name = attr
+                field_list.append(attr)
+                namespace[attr] = field
+        namespace['_field_list'] = field_list
         return super(ItemBuilder, cls).__new__(cls, name, base, namespace)
 
 
 class Item(object):
     __metaclass__ = ItemBuilder
 
-    def __init__(self, tree):
-        self.tree = tree
+    def __init__(self, tree, grab=None, task=None):
+        self._tree = tree
+        self._cache = {}
+        self._grab = grab
+        self._task = task
+        # TODO: Remove this hack
+        if grab:
+            self._tree.grab = grab
+
+    def _parse(self):
+        pass
+
+    def _render(self):
+        out = []
+        for key in self._field_list:
+            out.append('%s: %s' % (key, getattr(self, key)))
+        return '\n'.join(out)
+
--- test/item.py
+++ test/item.py
@@ -1,9 +1,16 @@
 # coding: utf-8
 from unittest import TestCase
 
+import os
+import sys
+root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, root)
+
 from grab import Grab, DataNotFound
-from grab.item import Item, IntegerField, StringField, DateTimeField
+from grab.item import (Item, IntegerField, StringField, DateTimeField, func_field,
+                       FuncField)
 from test.util import GRAB_TRANSPORT
+from grab.tools.lxml_tools import get_node_text
 
 XML = """<?xml version='1.0' encoding='utf-8'?>
 <bbapi version='1'>
@@ -20,6 +27,14 @@ XML = """<?xml version='1.0' encoding='utf-8'?>
 </bbapi>
 """
 
+def calculated_func2(self, tree):
+    if not hasattr(self, 'count2'):
+        self.count2 = 1
+    else:
+        self.count2 += 1
+    return get_node_text(tree.xpath('//height')[0]) + '-zoo2-' + str(self.count2)
+
+
 class Player(Item):
     id = IntegerField('//player/@id')
     first_name = StringField('//player/firstname')
@@ -28,6 +43,17 @@ class Player(Item):
     comment_cdata = StringField('//player/comment_cdata')
 
     data_not_found = StringField('//data/no/found')
+
+    @func_field
+    def calculated(self, tree):
+        if not hasattr(self, 'count'):
+            self.count = 1
+        else:
+            self.count += 1
+        return get_node_text(tree.xpath('//height')[0]) + '-zoo-' + str(self.count)
+
+    calculated2 = FuncField(calculated_func2)
+
 
 class TestItems(TestCase):
     def test_container_base_behavior(self):
@@ -39,6 +65,18 @@ class TestItems(TestCase):
         self.assertEquals(26982032, player.id)
         self.assertEquals('Ardeshir', player.first_name)
         self.assertEquals('2012-09-11 07:38:44', str(player.retrieved))
+        self.assertEquals('75-zoo-1', player.calculated)
+        # should got from cache
+        self.assertEquals('75-zoo-1', player.calculated)
+
+        # test assigning value
+        player.calculated = 'baz'
+        self.assertEquals('baz', player.calculated)
+
+        # test FuncField
+        self.assertEquals('75-zoo2-1', player.calculated2)
+        # should got from cache
+        self.assertEquals('75-zoo2-1', player.calculated2)
 
         # By default comment_cdata attribute contains empty string
         # because HTML DOM builder is used by default
