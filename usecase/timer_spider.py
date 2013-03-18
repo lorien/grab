6696ff2b4973dfab099b34181c0ac4723e59910f
Grigoriy Petukhov <lorien@lorien.name>
2013-03-19 02:36:10 +0700

    Add timers
    --HG--
    branch : ng2

--- usecase/timer_spider.py
+++ usecase/timer_spider.py
@@ -6,16 +6,18 @@ import logging
 
 class FooSpider(Spider):
     def task_generator(self):
-        for x in xrange(2):
-            yield Task('page', 'http://dumpz.org/%d/' % x)
+        yield Task('page', 'http://dumpz.org/100/')
+        yield Task('page', 'http://dumpz.org/101/', disable_cache=True)
 
     def task_page(self, grab, task):
         print task.url
-        time.sleep(1)
+        time.sleep(0.2)
+
 
 logging.basicConfig(level=logging.DEBUG)
 skip_generator = 'skip' in ''.join(sys.argv)
 bot = FooSpider(skip_generator=skip_generator, thread_number=5)
 bot.setup_queue(backend='mongo', database='test', queue_name='skip_test')
+bot.setup_cache(backend='mongo', database='test')
 bot.run()
 print bot.render_stats()
