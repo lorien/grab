d4cc351be0d942d8ae092935f9f9d964fea71636
Grigoriy Petukhov <lorien@lorien.name>
2013-06-08 23:28:45 +0700

    mongo queue: drop clear_on_init & clear_on_exit options, change method of generation of collection name if it was not defined explicitly

--- grab/spider/queue_backend/mongo.py
+++ grab/spider/queue_backend/mongo.py
@@ -12,35 +12,23 @@ from .base import QueueInterface
 logger = logging.getLogger('grab.spider.queue_backend.mongo')
 
 class QueueBackend(QueueInterface):
-    def __init__(self, database, queue_name=None, clear_on_init=False,
-                 clear_on_exit=False, **kwargs):
+    def __init__(self, spider_name, database=None, queue_name=None,
+                 **kwargs):
         """
         All "unexpected" kwargs goes to `pymongo.Connection()` method
         """
         if queue_name is None:
-            queue_name = 'queue_%s' % str(uuid.uuid4()).replace('-', '')
-            clear_on_init = False
-            clear_on_exit = True
-            logger.debug('Generating random task queue name: %s' % queue_name)
+            queue_name = 'task_queue_%s' % spider_name
 
         self.database = database
         self.queue_name = queue_name
         conn = pymongo.Connection(**kwargs)
         self.collection = conn[self.database][self.queue_name]
         logger.debug('Using collection: %s' % self.collection)
-        self.clear_on_init = clear_on_init
-        self.clear_on_exit = clear_on_exit
-
-        if self.clear_on_init:
-            self.clear_collection()
 
         self.collection.ensure_index('priority')
 
         super(QueueInterface, self).__init__(**kwargs)
-
-    def __del__(self):
-        if self.clear_on_exit:
-            self.clear_collection()
 
     def clear_collection(self):
         logger.debug('Deleting collection: %s' % self.collection)
