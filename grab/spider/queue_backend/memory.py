dc16540b5a4a83fffefec382f7ab98f581c3c4ef
Grigoriy Petukhov <lorien@lorien.name>
2013-06-20 01:38:08 +0700

    Remove unique feature from spider memory task queue

--- grab/spider/queue_backend/memory.py
+++ grab/spider/queue_backend/memory.py
@@ -4,25 +4,15 @@ from .base import QueueInterface
 from Queue import PriorityQueue, Empty
 
 class QueueBackend(QueueInterface):
-    def __init__(self, spider_name, unique=False, **kwargs):
+    def __init__(self, spider_name, **kwargs):
         super(QueueInterface, self).__init__(**kwargs)
         self.queue_object = PriorityQueue()
-        self.unique = unique
-        self.unique_dict = {}
 
     def put(self, task, priority):
-        if self.unique:
-            key = unique_key(task)
-            if key in self.unique_dict:
-                return
-            self.unique_dict[key] = True
         self.queue_object.put((priority, task))
 
     def get(self, timeout):
         priority, task = self.queue_object.get(True, timeout)
-        if self.unique:
-            key = unique_key(task)
-            del self.unique_dict[key]
         return task
 
     def size(self):
@@ -34,6 +24,3 @@ class QueueBackend(QueueInterface):
                 self.queue_object.get(False)
         except Empty:
             pass
-
-def unique_key(task):
-    return '%s:%s' % (task.name, task.url)
