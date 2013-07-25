7126822a5d9e5c1bfee9821c72081f7914afeeca
Gregory Petukhov <lorien@lorien.name>
2013-07-25 16:15:08 +0700

    Version 0.4.12

--- grab/__init__.py
+++ grab/__init__.py
@@ -4,5 +4,5 @@ from .base import UploadContent, UploadFile
 from .base import Grab
 from .tools.logs import default_logging
 
-version_info = (0, 4, 11)
+version_info = (0, 4, 12)
 __version__ = '.'.join(map(str, version_info))
