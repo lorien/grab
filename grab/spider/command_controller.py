cbdb7f2ccff46cd90ba046c0c6f09950abe9d585
Gregory Petukhov <lorien@lorien.name>
2013-08-10 21:05:22 +0700

    Star working on command controller

---
+++ grab/spider/command_controller.py
@@ -0,0 +1,8 @@
+class CommandController(object):
+    def __init__(self, spider):
+        self.spider = spider
+        self.enabled = False
+
+    #def add_interface(backend, **kwargs):
+
+    #def 
