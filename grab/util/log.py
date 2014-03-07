2c5383bc3c9eb4757ce305e073c109c99df9600b
Gregory Petukhov <lorien@lorien.name>
2014-03-07 02:13:42 +0000

    Fix bug in grab.tools.log:print_dict

--- grab/util/log.py
+++ grab/util/log.py
@@ -1,10 +1,14 @@
+"""
+This module contains `print_dict` function that is useful
+to dump content of dictionary in human acceptable representation.
+"""
 def repr_value(val):
     if isinstance(val, unicode):
         return val.encode('utf-8')
     elif isinstance(val, (list, tuple)):
-        return '[%s]' % ', '.join(repr_val(x) for x in val)
+        return '[%s]' % ', '.join(repr_value(x) for x in val)
     elif isinstance(val, dict):
-        return '{%s}' % ', '.join('%s: %s' % (repr_val(x), repr_val(y)) for x, y in val.items())
+        return '{%s}' % ', '.join('%s: %s' % (repr_value(x), repr_value(y)) for x, y in val.items())
     else:
         return str(val)
 
