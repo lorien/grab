5bda307ce204a27f03316652a79131685b2e5fb2
Gregory Petukhov <lorien@lorien.name>
2014-04-02 04:25:48 +0000

    Add allowed_dot argument to slugify function. Write more tests for slugify function

--- grab/tools/russian.py
+++ grab/tools/russian.py
@@ -11,21 +11,35 @@ class InvalidMonthName(Exception):
 
 RE_NOT_ENCHAR = re.compile(u'[^-a-zA-Z0-9]', re.U)
 RE_NOT_ENRUCHAR = re.compile(u'[^-a-zA-Zа-яА-ЯёЁ0-9]', re.U)
-RE_RUSSIAN_CHAR = re.compile(u'[а-яА-ЯёЁ]', re.U)
+
+RE_NOT_ENCHAR_DOT = re.compile(u'[^-.a-zA-Z0-9]', re.U)
+RE_NOT_ENRUCHAR_DOT = re.compile(u'[^-.a-zA-Zа-яА-ЯёЁ0-9]', re.U)
+
 RE_DASH = re.compile(r'-+')
 
-def slugify(value, limit=None, default='', lower=True):
+
+def slugify(value, limit=None, default='', lower=True, dot_allowed=False):
     value = smart_unicode(value)
 
-    # Replace all non russian/english chars with "-" char
+    # Replace all non-allowed chars with "-" char
     # to help pytils not to crash
-    value = RE_NOT_ENRUCHAR.sub('-', value)
+    if dot_allowed:
+        value = RE_NOT_ENRUCHAR_DOT.sub('-', value)
+    else:
+        value = RE_NOT_ENRUCHAR.sub('-', value)
 
     # Do transliteration
     value = translify(value)
 
     # Replace trash with safe "-" char
-    value = RE_NOT_ENCHAR.sub('-', value).strip('-')
+    if dot_allowed:
+        value = RE_NOT_ENCHAR_DOT.sub('-', value)
+    else:
+        value = RE_NOT_ENCHAR.sub('-', value)
+    
+    # Replace "-" from both side of the string
+    value = value.strip('-')
+
     if lower:
         value = value.lower()
 
@@ -39,6 +53,7 @@ def slugify(value, limit=None, default='', lower=True):
         return value
     else:
         return default
+
 
 def parse_ru_month(val):
     names = (None, u'января', u'февраля', u'марта', u'апреля',
