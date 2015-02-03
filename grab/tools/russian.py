15d77480326598e4a4df3f910b6522ad202848ba
Gregory Petukhov <lorien@lorien.name>
2015-02-03 13:26:53 +0700

    Add delimiter option to grab.tools.russian::slugify

--- grab/tools/russian.py
+++ grab/tools/russian.py
@@ -15,36 +15,41 @@ RE_NOT_ENRUCHAR = re.compile(u'[^-a-zA-Zа-яА-ЯёЁ0-9]', re.U)
 RE_NOT_ENCHAR_DOT = re.compile(u'[^-.a-zA-Z0-9]', re.U)
 RE_NOT_ENRUCHAR_DOT = re.compile(u'[^-.a-zA-Zа-яА-ЯёЁ0-9]', re.U)
 
-RE_DASH = re.compile(r'-+')
+#RE_DASH = re.compile(r'-+')
 
 
-def slugify(value, limit=None, default='', lower=True, dot_allowed=False):
+def slugify(value, limit=None, default='', lower=True, dot_allowed=False,
+            delimiter='-'):
     value = smart_unicode(value)
 
     # Replace all non-allowed chars with "-" char
     # to help pytils not to crash
     if dot_allowed:
-        value = RE_NOT_ENRUCHAR_DOT.sub('-', value)
+        value = RE_NOT_ENRUCHAR_DOT.sub(delimiter, value)
     else:
-        value = RE_NOT_ENRUCHAR.sub('-', value)
+        value = RE_NOT_ENRUCHAR.sub(delimiter, value)
 
     # Do transliteration
     value = translify(value)
 
     # Replace trash with safe "-" char
     if dot_allowed:
-        value = RE_NOT_ENCHAR_DOT.sub('-', value)
+        value = RE_NOT_ENCHAR_DOT.sub(delimiter, value)
     else:
-        value = RE_NOT_ENCHAR.sub('-', value)
+        value = RE_NOT_ENCHAR.sub(delimiter, value)
     
     # Replace "-" from both side of the string
-    value = value.strip('-')
+    value = value.strip(delimiter)
 
     if lower:
         value = value.lower()
 
     # Replace sequences of dashes
-    value = RE_DASH.sub('-', value)
+    #value = RE_DASH.sub(delimiter, value)
+    value = value.replace(delimiter * 5, delimiter)
+    value = value.replace(delimiter * 4, delimiter)
+    value = value.replace(delimiter * 3, delimiter)
+    value = value.replace(delimiter * 2, delimiter)
 
     if limit is not None:
         value = value[:limit]
