a1443556b764b03ebcd1d54999d5857d151dc6f5
Gregory Petukhov <lorien@lorien.name>
2015-02-09 22:45:13 +0700

    Version 0.5.0

--- grab/__init__.py
+++ grab/__init__.py
@@ -4,5 +4,5 @@ from grab.upload import UploadContent, UploadFile
 from grab.base import Grab
 from grab.tools.logs import default_logging
 
-version_info = (0, 4, 13)
+version_info = (0, 5, 0)
 __version__ = '.'.join(map(str, version_info))
