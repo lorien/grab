d350292d5fc39a30a870e958b88aef4f7857e759
Gregory Petukhov <lorien@lorien.name>
2015-02-15 21:01:19 +0700

    Remove obsoleted files from repo

--- profile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# To run this tool you'll need:
-# * aptitude install python-profiler kcachegrind
-# * pip install  pyprof2calltree
-rm prof
-python -m cProfile -o prof $1
-pyprof2calltree -i prof -o prof.kcache
