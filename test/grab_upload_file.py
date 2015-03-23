05347a46e401ac67df9664cffa07840940917fd6
kalombo <ngorshkov@telenet.ru>
2015-03-23 16:02:35 +0500

    added upload file tests

--- test/grab_upload_file.py
+++ test/grab_upload_file.py
@@ -1,6 +1,8 @@
 # coding: utf-8
-from grab import UploadContent
-
+from grab import UploadContent, UploadFile
+import tempfile
+import os
+import pycurl
 from test.util import build_grab
 from test.util import BaseGrabTestCase
 
@@ -43,9 +45,20 @@ class TestUploadContent(BaseGrabTestCase):
         self.server.reset()
         # Create fake grab instance with fake response
         self.g = build_grab()
-        self.g.fake_response(FORMS, charset='utf-8')
+        self.g.setup_document(FORMS, charset='utf-8')
 
     def test(self):
         fc = UploadContent('a')
         self.assertEqual(fc, 'xxx')
-        self.g.set_input('image', fc)
+        self.assertEqual(fc.field_tuple(), (pycurl.FORM_CONTENTS, 'a'))
+        self.g.doc.set_input('image', fc)
+
+        fc, path = tempfile.mkstemp()
+        data = b'foo'
+        with open(path, 'wb') as out:
+            out.write(data)
+        fc = UploadFile(path)
+        self.assertEqual(fc, 'xxx')
+        self.g.doc.set_input('image', fc)
+        self.assertEqual(fc.field_tuple(), (pycurl.FORM_FILE, path))
+        os.unlink(path)
