9c685f08ee44a1318db9556371eab8e8e1c65c00
Gregory Petukhov <lorien@lorien.name>
2015-06-05 16:17:53 +0600

    Change default network/try limits

--- grab/util/default_config.py
+++ grab/util/default_config.py
@@ -2,8 +2,8 @@ DEFAULT_SPIDER_GLOBAL_CONFIG = {
     'spider_modules': ['spider'],
     'save_report': True,
     'thread_number': 1,
-    'network_try_limit': 10,
-    'task_try_limit': 10,
+    'network_try_limit': 3,
+    'task_try_limit': 3,
     'display_timing': False,
     'display_stats': True,
 }
