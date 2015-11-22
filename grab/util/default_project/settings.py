340cdf09b858ba45b949919af2cb3d84ef0cee2b
Gregory Petukhov <lorien@lorien.name>
2015-11-23 01:17:29 +0600

    Update default_project files

---
+++ grab/util/default_project/settings.py
@@ -0,0 +1,23 @@
+GRAB_SPIDER_MODULES = ['spider']
+
+GRAB_PROXY_LIST = {
+    'source': '/web/proxy.txt',
+    'source_type': 'text_file',
+}
+
+# GRAB_CACHE = {
+#    'backend': 'mysql',
+#    'database': '{{ PROJECT_NAME }}_cache',
+#    'user': '',
+#    'passwd': '',
+# }
+
+# GRAB_THREAD_NUMBER = 15
+
+# GRAB_QUEUE = {
+#    'backend': 'redis',
+# }
+
+# GRAB_TASK_REFRESH_CACHE = {
+#    'foo': True,
+# }
---
+++ grab/util/default_project/spider.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+# coding: utf-8
+from grab.spider import Spider, Task
+from weblib.logs import default_logging
+from grab import Grab
+import logging
+
+from database import db
+
+
+class {{ PROJECT_NAME_CAMELCASE }}Spider(Spider):  # noqa
+    def task_generator(self):
+        yield Task('initial', url='')
+
+    def task_initial(self, grab, task):
+        pass
