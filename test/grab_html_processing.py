0c2b0363c77ee2161633c73aa1a658d04cc8389d
Gregory Petukhov <lorien@lorien.name>
2017-02-12 21:24:43 +0700

    test 1

--- test/grab_html_processing.py
+++ test/grab_html_processing.py
@@ -34,10 +34,11 @@ class GrabHTMLProcessingTestCase(BaseGrabTestCase):
         re_emoji = re.compile(
             "["
             u"\U0001F600-\U0001F64F"  # emoticons
-            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-            u"\U0001F680-\U0001F6FF"  # transport & map symbols
-            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
             "]+", flags=re.UNICODE)
+            #u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            #u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            #u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            #"]+", flags=re.UNICODE)
 
         for fname in glob('test/files/dump/*.html'):
             print('Parsing %s document' % fname)
