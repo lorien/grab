f497edcbac19248e31b568a7f260a180be897769
Gregory Petukhov <lorien@lorien.name>
2017-02-12 12:58:43 +0700

    Add extra test with stripped emoji

--- test/grab_html_processing.py
+++ test/grab_html_processing.py
@@ -23,3 +23,28 @@ class GrabHTMLProcessingTestCase(BaseGrabTestCase):
             g.go(self.server.get_url())
             for elem in g.doc('//*'):
                 elem.text()
+
+    def test_parse_cleaned_document(self):
+        """
+        Same as test_parse_document
+        but documents are cleared from emoji
+        """
+
+        import re
+        re_emoji = re.compile(
+            "["
+            u"\U0001F600-\U0001F64F"  # emoticons
+            u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+            u"\U0001F680-\U0001F6FF"  # transport & map symbols
+            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+            "]+", flags=re.UNICODE)
+
+        for fname in glob('test/files/dump/*.html'):
+            print('Parsing %s document' % fname)
+            g = build_grab()
+            data = open(fname, 'rb').read()
+            data = re_emoji.sub('', data.decode('utf-8')).encode('utf-8')
+            self.server.response['data'] = data
+            g.go(self.server.get_url())
+            for elem in g.doc('//*'):
+                elem.text()
