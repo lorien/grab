72c9e5599ef83ea93562ce6e7d016e832c50eede
Gregory Petukhov <lorien@lorien.name>
2017-02-26 15:20:14 +0700

    Improve meta info in setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,37 @@
 import os
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 ROOT = os.path.dirname(os.path.realpath(__file__))
 
 setup(
+    # Meta data
     name='grab',
     version='0.6.36',
-    description='Web Scraping Framework',
-    long_description=open(os.path.join(ROOT, 'README.rst')).read(),
-    url='http://grablib.org',
     author='Gregory Petukhov',
     author_email='lorien@lorien.name',
-
-    packages=find_packages(exclude=['test', 'test.files']),
+    maintainer='Gregory Petukhov',
+    maintainer_email='lorien@lorien.name',
+    url='http://grablib.org',
+    description='Web Scraping Framework',
+    long_description=open(os.path.join(ROOT, 'README.rst')).read(),
+    download_url='https://pypi.python.org/pypi/grab',
+    keywords='pycurl multicurl curl network parsing grabbing scraping'
+             ' lxml xpath data mining',
+    license='MIT License',
+    # Package files
+    packages=[
+        'grab',
+        'grab.script',
+        'grab.spider',
+        'grab.spider.cache_backend',
+        'grab.spider.queue_backend',
+        'grab.spider.transport',
+        'grab.transport',
+        'grab.util',
+    ],
     include_package_data=True,
+    # Dependencies
     install_requires=[
         'weblib>=0.1.23',
         'six',
@@ -27,14 +44,14 @@ setup(
     extras_require={
         'full': ['urllib3'],
     },
-
-    license='MIT',
-    keywords='pycurl multicurl curl network parsing grabbing scraping'
-             ' lxml xpath data mining',
+    # Topics
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'License :: OSI Approved :: MIT License',
         'Development Status :: 5 - Production/Stable',
