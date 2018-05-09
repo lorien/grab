88ab277ff523ed99fa9c9a4b2a50a34dd144bf96
Gregory Petukhov <lorien@lorien.name>
2018-05-10 00:13:17 +0300

    Bump version: 0.6.38 → 0.6.39

--- grab/__init__.py
+++ grab/__init__.py
@@ -7,5 +7,5 @@ from grab.error import (GrabError, DataNotFound, GrabNetworkError,  # noqa
 from grab.upload import UploadContent, UploadFile  # noqa
 from grab.base import Grab  # noqa
 
-__version__ = '0.6.38'
+__version__ = '0.6.39'
 VERSION_NUMERIC = tuple(map(int, __version__.split('.')))
