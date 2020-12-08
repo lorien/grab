2a59d20579193ef9daa2f6fae2ce2bddeaf22beb
Some User <guest@example.com>
2020-12-08 04:54:40 +0300

    Fix github CI (py2.7 issue)

--- tests/grab_redirect.py
+++ tests/grab_redirect.py
@@ -1,7 +1,5 @@
 # coding: utf-8
-from urllib.parse import unquote
-
-from six.moves.urllib.parse import quote
+from six.moves.urllib.parse import quote, unquote
 
 from grab.error import GrabTooManyRedirectsError
 from tests.util import BaseGrabTestCase, build_grab
