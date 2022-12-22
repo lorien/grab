14765baa26faefa202f715823746a2a1fb8e87fe
Some User <guest@example.com>
2022-12-22 23:04:18 +0300

    Update timeout tests

--- tests/test_grab_timeout.py
+++ tests/test_grab_timeout.py
@@ -9,13 +9,13 @@ class GrabTimeoutCase(BaseGrabTestCase):
         self.server.reset()
 
     def test_timeout_raises(self):
-        self.server.add_response(Response(data=b"zzz", sleep=0.2))
+        self.server.add_response(Response(data=b"zzz", sleep=1))
         grab = build_grab()
         grab.setup(timeout=0.1, url=self.server.get_url())
         self.assertRaises(GrabTimeoutError, grab.request)
 
     def test_timeout_enough_to_complete(self):
-        self.server.add_response(Response(data=b"zzz", sleep=0.2))
+        self.server.add_response(Response(data=b"zzz", sleep=0.5))
         grab = build_grab()
-        grab.setup(timeout=1, url=self.server.get_url())
+        grab.setup(timeout=2, url=self.server.get_url())
         grab.request()
