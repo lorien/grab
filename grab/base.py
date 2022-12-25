acb81a816ef28bc93ab0d6d6dd34d598626ca5b4
Some User <guest@example.com>
2022-12-25 05:25:07 +0300

    Remove dead code from grab.base module

--- grab/base.py
+++ grab/base.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
 
-import os
-import tempfile
 from abc import ABCMeta, abstractmethod
-from collections.abc import Generator, Mapping, MutableMapping
+from collections.abc import Generator, MutableMapping
 from contextlib import contextmanager
 from http.cookiejar import CookieJar
-from typing import Any, Optional, cast, overload
+from typing import Any, overload
 
 from grab.document import Document
 from grab.request import Request
@@ -90,31 +88,3 @@ class BaseTransport(metaclass=ABCMeta):
     @abstractmethod
     def request(self, req: Request, cookiejar: CookieJar) -> None:  # pragma: no cover
         raise NotImplementedError
-
-    def setup_body_file(
-        self,
-        storage_dir: str,
-        storage_filename: None | str,
-        create_dir: bool = False,
-    ) -> str:
-        if create_dir and not os.path.exists(storage_dir):
-            os.makedirs(storage_dir)
-        if storage_filename is None:
-            file, file_path = tempfile.mkstemp(dir=storage_dir)
-            os.close(file)
-            return file_path
-        return os.path.join(storage_dir, storage_filename)
-
-    def detect_request_method(self, grab_config: Mapping[str, Any]) -> str:
-        """Analyze request config and find which request method will be used.
-
-        Returns request method in upper case
-        """
-        # pylint: disable=consider-alternative-union-syntax
-        method = cast(Optional[str], grab_config["method"])
-        # pylint: enable=consider-alternative-union-syntax
-        if method:
-            return method.upper()
-        if grab_config["body"] or grab_config["fields"]:
-            return "POST"
-        return "GET"
