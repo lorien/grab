4698aba3b70a221a47d39913b7456503f46f52c7
User <guest@example.com>
2025-09-08 23:38:19 +0300

    Start working on fixing tests to run on recent test_server package

---
+++ fix_test.py
@@ -0,0 +1,109 @@
+import re
+from glob import glob
+
+# self.server.response["get.data"] = "Simple String"
+
+RE_GET_DATA = re.compile(
+    r'^(\s+)self\.server\.response(_once)?\[["\']get\.data["\']\]\s*=\s*["\'](.+?)["\']',
+    re.M,
+)
+RE_HEADERS = re.compile(
+    r'^(\s+)self\.server\.response(_once)?\[["\']headers["\']\]\s*=\s*(\[[^\]]+\])',
+    re.M,
+)
+RE_COOKIES_DICT = re.compile(
+    r'^(\s+)self\.server\.response(_once)?\[["\']cookies["\']\]\s*=\s*(\{[^\}]+\}\.items\(\))',
+    re.M,
+)
+RE_COOKIES_DICT_EMPTY = re.compile(
+    r'^(\s+)self\.server\.response(_once)?\[["\']cookies["\']\]\s*=\s*(\{\})',
+    re.M,
+)
+RE_COOKIES_LIST = re.compile(
+    r'^(\s+)self\.server\.response(_once)?\[["\']cookies["\']\]\s*=\s*(\[[^\]]*\])',
+    re.M,
+)
+RE_CODE = re.compile(
+    r'^(\s+)self\.server\.response(_once)?\[["\']code["\']\]\s*=\s*(\d+)',
+    re.M,
+)
+RE_IMPORT = re.compile(r"^(from tests\.util[^\n]+BaseGrabTestCase)", re.M)
+
+# self.server.response['cookies'] = {'foo': 'bar', '1': '2'}.items()
+
+
+def handler_get_data(match):
+    # fmt: off
+    #print(u"Fixing line: {}".format(match.group(0)))
+    return u'{}self.server.add_response(Response(data="{}"), count=1, method="get")'.format(
+        match.group(1),
+        match.group(3),
+    )
+    # fmt: on
+
+
+def handler_headers(match):
+    # fmt: off
+    #print(u"Fixing line: {}".format(match.group(0)))
+    return u'{}self.server.add_response(Response(headers={}), count=1)'.format(
+        match.group(1),
+        match.group(3),
+    )
+    # fmt: on
+
+
+def handler_cookies(match):
+    # fmt: off
+    #print(u"Fixing line: {}".format(match.group(0)))
+    return u'{}self.server.add_response(Response(cookies={}), count=1)'.format(
+        match.group(1),
+        match.group(3),
+    )
+    # fmt: on
+
+
+def handler_code(match):
+    # fmt: off
+    #print(u"Fixing line: {}".format(match.group(0)))
+    return u'{}self.server.add_response(Response(code={}), count=1)'.format(
+        match.group(1),
+        match.group(3),
+    )
+    # fmt: on
+
+
+def handler_import(match):
+    # fmt: off
+    print(u"Fixing line: {}".format(match.group(0)))
+    return u'from test_server import Request, Response\n{}'.format(
+        match.group(1),
+    )
+    # fmt: on
+
+
+def process_file(path):
+    print("--- {}".format(path))
+    with open(path, "rb") as inp:
+        content = inp.read().decode("utf-8")
+        new_content = content
+        new_content = RE_GET_DATA.sub(handler_get_data, new_content)
+        new_content = RE_HEADERS.sub(handler_headers, new_content)
+        new_content = RE_COOKIES_DICT.sub(handler_cookies, new_content)
+        new_content = RE_COOKIES_DICT_EMPTY.sub(handler_cookies, new_content)
+        new_content = RE_COOKIES_LIST.sub(handler_cookies, new_content)
+        new_content = RE_CODE.sub(handler_code, new_content)
+        if new_content != content:
+            new_content = RE_IMPORT.sub(handler_import, new_content)
+            new_path = path.replace("tests/", "fix/")
+            with open(new_path, "wb") as out:
+                out.write(new_content.encode("utf-8"))
+
+
+def main():
+    for path in glob("tests/*.py"):
+        if "__init__" not in path:
+            process_file(path)
+
+
+if __name__ == "__main__":
+    main()
--- tests/grab_get_request.py
+++ tests/grab_get_request.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from tests.util import build_grab
+from test_server import Request, Response
 from tests.util import BaseGrabTestCase
 
 
@@ -8,26 +9,26 @@ class GrabSimpleTestCase(BaseGrabTestCase):
         self.server.reset()
 
     def test_get(self):
-        self.server.response['get.data'] = 'Final Countdown'
+        self.server.add_response(Response(data="Final Countdown"), count=1, method="get")
         grab = build_grab()
         grab.go(self.server.get_url())
         self.assertTrue(b'Final Countdown' in grab.doc.body)
 
     def test_body_content(self):
-        self.server.response['get.data'] = 'Simple String'
+        self.server.add_response(Response(data="Simple String"), count=1, method="get")
         grab = build_grab()
         grab.go(self.server.get_url())
         self.assertEqual(b'Simple String', grab.doc.body)
         # self.assertEqual('Simple String' in grab.doc.runtime_body)
 
     def test_status_code(self):
-        self.server.response['get.data'] = 'Simple String'
+        self.server.add_response(Response(data="Simple String"), count=1, method="get")
         grab = build_grab()
         grab.go(self.server.get_url())
         self.assertEqual(200, grab.doc.code)
 
     def test_parsing_response_headers(self):
-        self.server.response['headers'] = [('Hello', 'Grab')]
+        self.server.add_response(Response(headers=[('Hello', 'Grab')]), count=1)
         grab = build_grab()
         grab.go(self.server.get_url())
         self.assertTrue(grab.doc.headers['Hello'] == 'Grab')
