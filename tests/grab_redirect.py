1ac7a4882195e13d015682637570994f90a86d46
User <guest@example.com>
2025-09-11 19:55:58 +0300

    Fix redirect tests

--- tests/grab_redirect.py
+++ tests/grab_redirect.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+from pprint import pprint  # pylint: disable=unused-import # noqa: F401
+
 from six.moves.urllib.parse import quote
 
 from grab.error import GrabTooManyRedirectsError
@@ -16,18 +18,19 @@ def build_location_callback(url, counter):
         if meta["counter"]:
             status = 301
             headers = [("Location", meta["url"])]
-            body = b""
+            data = b""
         else:
             status = 200
             headers = []
-            body = b"done"
+            data = b"done"
         meta["counter"] -= 1
-        return {
+        res = {
             "type": "response",
             "status": status,
-            "body": body,
+            "data": data,
             "headers": headers,
         }
+        return res
 
     return callback
 
@@ -41,12 +44,12 @@ def build_refresh_callback(url, counter):
     def callback():
         if meta["counter"]:
             status = 200
-            body = b"<html><head><meta " b'http-equiv="refresh" content="5"></head>'
+            data = b"<html><head><meta " b'http-equiv="refresh" content="5"></head>'
         else:
             status = 200
-            body = b"done"
+            data = b"done"
         meta["counter"] -= 1
-        return {"type": "response", "status": status, "body": body}
+        return {"type": "response", "status": status, "data": data}
 
     return callback
 
@@ -56,104 +59,102 @@ class GrabRedirectTestCase(BaseGrabTestCase):
         self.server.reset()
 
     def test_follow_refresh_off(self):
-        # By default meta-redirect is off
-        meta_url = self.server.get_url("/foo")
-
-        self.server.add_response(
-            Response(data='<meta http-equiv="refresh" content="5; url=%s">' % meta_url),
-            count=1,
-            method="get",
-        )
-        grab = build_grab()
-        grab.go(self.server.get_url())
-        req = self.server.get_request()
-        self.assertEqual(req.path, "/")
-        self.assertEqual(grab.doc.url, self.server.get_url())
+       # By default meta-redirect is off
+       meta_url = self.server.get_url("/foo")
+       self.server.add_response(
+           Response(data='<meta http-equiv="refresh" content="5; url=%s">' % meta_url),
+           count=1,
+       )
+       grab = build_grab()
+       grab.go(self.server.get_url())
+       req = self.server.get_request()
+       self.assertEqual(req.path, "/")
+       self.assertEqual(grab.doc.url, self.server.get_url("/"))
 
     def test_follow_refresh_on(self):
-        meta_url = self.server.get_url("/foo")
-        # Now test meta-auto-redirect
-        self.server.add_response(
-            Response(data='<meta http-equiv="refresh" content="5; url=%s">' % meta_url),
-            count=1,
-            method="get",
-        )
-        grab = build_grab()
-        grab.setup(follow_refresh=True)
-        grab.go(self.server.get_url())
-        req = self.server.get_request()
-        self.assertEqual(req.path, "/foo")
-        self.assertEqual(grab.doc.url, meta_url)
+       meta_url = self.server.get_url("/foo")
+       # Now test meta-auto-redirect
+       self.server.add_response(
+           Response(data='<meta http-equiv="refresh" content="5; url=%s">' % meta_url),
+           count=1,
+       )
+       self.server.add_response(Response())
+       grab = build_grab()
+       grab.setup(follow_refresh=True)
+       grab.go(self.server.get_url())
+       req = self.server.get_request()
+       self.assertEqual(req.path, "/foo")
+       self.assertEqual(grab.doc.url, meta_url)
 
     def test_spaces_in_refresh_url(self):
-        meta_url = self.server.get_url("/foo")
-        # Test spaces in meta tag
+       meta_url = self.server.get_url("/foo")
+       # Test spaces in meta tag
+       self.server.add_response(
+           Response(data="<meta http-equiv='refresh' content='0;url= %s'>" % meta_url),
+           count=1,
+           method="get",
+       )
+       self.server.add_response(Response(data="ok"))
+       grab = build_grab()
+       grab.setup(follow_refresh=True)
+       grab.go(self.server.get_url())
+       req = self.server.get_request()
+       self.assertEqual(req.path, "/foo")
+       self.assertEqual(grab.doc.url, meta_url)
+
+    def test_refresh_redirect_limit(self):
+       self.server.add_response(Response(callback=build_refresh_callback(
+           self.server.get_url(), 10
+       )), count=-1)
+
+       grab = build_grab()
+       grab.setup(redirect_limit=10, follow_refresh=True)
+       grab.go(self.server.get_url())
+       self.assertTrue(b"done" in grab.doc.body)
+
+       self.server.add_response(Response(callback = build_refresh_callback(
+           self.server.get_url(), 10
+       )), count=-1)
+       grab.setup(redirect_limit=5, follow_refresh=True)
+       self.assertRaises(
+           GrabTooManyRedirectsError, lambda: grab.go(self.server.get_url())
+       )
+
+    def test_redirect_limit(self):
         self.server.add_response(
-            Response(data="<meta http-equiv='refresh' content='0;url= %s'>" % meta_url),
-            count=1,
-            method="get",
+            Response(callback=build_location_callback(self.server.get_url(), 10)),
+            count=-1,
         )
-        self.server.add_response(Response(data="ok"))
+
         grab = build_grab()
-        grab.setup(follow_refresh=True)
+        grab.setup(redirect_limit=5)
+
+        with self.assertRaises(GrabTooManyRedirectsError):
+            grab.go(self.server.get_url())
+
+        self.server.add_response(Response(callback=build_location_callback(
+            self.server.get_url(), 10
+        )))
+        grab.setup(redirect_limit=20)
         grab.go(self.server.get_url())
-        req = self.server.get_request()
-        self.assertEqual(req.path, "/foo")
-        self.assertEqual(grab.doc.url, meta_url)
+        self.assertTrue(b"done" in grab.doc.body)
 
-    # def test_refresh_redirect_limit(self):
-    #    self.server.response["get.callback"] = build_refresh_callback(
-    #        self.server.get_url(), 10
-    #    )
-
-    #    grab = build_grab()
-    #    grab.setup(redirect_limit=10, follow_refresh=True)
-    #    grab.go(self.server.get_url())
-    #    self.assertTrue(b"done" in grab.doc.body)
-
-    #    self.server.response["get.callback"] = build_refresh_callback(
-    #        self.server.get_url(), 10
-    #    )
-    #    grab.setup(redirect_limit=5, follow_refresh=True)
-    #    self.assertRaises(
-    #        GrabTooManyRedirectsError, lambda: grab.go(self.server.get_url())
-    #    )
-
-    # def test_redirect_limit(self):
-    #    self.server.response["get.callback"] = build_location_callback(
-    #        self.server.get_url(), 10
-    #    )
-
-    #    grab = build_grab()
-    #    grab.setup(redirect_limit=5)
-
-    #    self.assertRaises(
-    #        GrabTooManyRedirectsError, lambda: grab.go(self.server.get_url())
-    #    )
-
-    #    self.server.response["get.callback"] = build_location_callback(
-    #        self.server.get_url(), 10
-    #    )
-    #    grab.setup(redirect_limit=20)
-    #    grab.go(self.server.get_url())
-    #    self.assertTrue(b"done" in grab.doc.body)
 
     def test_redirect_utf_location(self):
-        self.server.add_response(Response(status=301), count=1)
-        # fmt: off
-        self.server.add_response(
-            Response(
-                headers=[
-                    ("Location", (self.server.get_url() + u"фыва").encode("utf-8")),
-                ]
-            ),
-            count=1,
-        )
-        # fmt: on
-        self.server.add_response(Response(data="content-1"), count=1)
-        self.server.add_response(Response(data="content-2"), count=1)
-        grab = build_grab(debug=True, follow_location=True)
-        grab.go(self.server.get_url())
-        # fmt: off
-        self.assertTrue(quote(u"/фыва".encode("utf-8"), safe="/") in grab.doc.url)
-        # fmt: on
+       # fmt: off
+       self.server.add_response(
+           Response(
+               status=301,
+               headers=[
+                   ("Location", (self.server.get_url() + u"/фыва").encode("utf-8")),
+               ]
+           ),
+           count=1,
+       )
+       # fmt: on
+       self.server.add_response(Response(), count=1)
+       grab = build_grab(debug=True, follow_location=True)
+       grab.go(self.server.get_url())
+       # fmt: off
+       self.assertTrue(quote(u"/фыва".encode("utf-8"), safe="/") in grab.doc.url)
+       # fmt: on
