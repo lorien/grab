e872cd594ed9f2a453b4a60668c4f9116507e566
User <guest@example.com>
2025-09-13 15:46:32 +0300

    Use fastrq instead of qr in redis queue. Rename queue backend files.

--- grab/spider/cache_service.py
+++ grab/spider/cache_service.py
@@ -1,9 +1,9 @@
 import time
 
-from six.moves.queue import Queue, Empty
+from six.moves.queue import Empty, Queue
 
 from grab.spider.base_service import BaseService
-from grab.spider.queue_backend import memory
+from grab.spider.queue_backend import memory_queue
 
 
 class CacheServiceBase(BaseService):
@@ -24,7 +24,7 @@ class CacheServiceBase(BaseService):
 
 class CacheReaderService(CacheServiceBase):
     def create_input_queue(self):
-        return memory.QueueBackend(spider_name=None)
+        return memory_queue.QueueBackend(spider_name=None)
 
     def worker_callback(self, worker):
         while not worker.stop_event.is_set():
@@ -46,33 +46,33 @@ class CacheReaderService(CacheServiceBase):
                             (result, task, None)
                         )
                 if not item:
-                    self.spider.task_dispatcher.input_queue.put((
-                        task, None, {'source': 'cache_reader'}
-                    ))
+                    self.spider.task_dispatcher.input_queue.put(
+                        (task, None, {"source": "cache_reader"})
+                    )
 
     def is_read_allowed(self, task, grab):
         return (
-            not task.get('refresh_cache', False)
-            and not task.get('disable_cache', False)
-            and grab.detect_request_method() == 'GET'
+            not task.get("refresh_cache", False)
+            and not task.get("disable_cache", False)
+            and grab.detect_request_method() == "GET"
         )
 
     def load_from_cache(self, task, grab):
-        cache_item = self.backend.get_item(grab.config['url'])
+        cache_item = self.backend.get_item(grab.config["url"])
         if cache_item is None:
-            self.spider.stat.inc('cache:req-miss')
+            self.spider.stat.inc("cache:req-miss")
         else:
             grab.prepare_request()
             self.backend.load_response(grab, cache_item)
-            grab.log_request('CACHED')
-            self.spider.stat.inc('cache:req-hit')
+            grab.log_request("CACHED")
+            self.spider.stat.inc("cache:req-hit")
 
             return {
-                'ok': True,
-                'grab': grab,
-                'grab_config_backup': grab.dump_config(),
-                'emsg': None,
-                'from_cache': True,
+                "ok": True,
+                "grab": grab,
+                "grab_config_backup": grab.dump_config(),
+                "emsg": None,
+                "from_cache": True,
             }, task
 
 
@@ -93,9 +93,7 @@ class CacheWriterService(CacheServiceBase):
 
     def is_write_allowed(self, task, grab):
         return (
-            grab.request_method == 'GET'
-            and not task.get('disable_cache')
-            and self.spider.is_valid_network_response_code(
-                grab.doc.code, task
-            )
+            grab.request_method == "GET"
+            and not task.get("disable_cache")
+            and self.spider.is_valid_network_response_code(grab.doc.code, task)
         )
--- grab/spider/queue_backend/redis_queue.py
+++ grab/spider/queue_backend/redis_queue.py
@@ -5,13 +5,33 @@ try:
     import Queue as queue
 except ImportError:
     import queue
-import random
+
 import logging
+import pickle
+import random
 
-from qr import PriorityQueue
+from fastrq.priorityqueue import PriorityQueue
+from redis import StrictRedis
 
-from grab.spider.queue_backend.base import QueueInterface
 from grab.spider.error import SpiderMisuseError
+from grab.spider.queue_backend.base import QueueInterface
+
+
+class CustomPriorityQueue(PriorityQueue):
+    def __init__(self, key, **kwargs):
+        # sets `key` to `self._key`
+        super(CustomPriorityQueue, self).__init__(key)
+        self._conn_kwargs = kwargs
+
+    # https://github.com/limen/fastrq/blob/master/fastrq/base.py
+    def connect(self):
+        if self._redis is None:
+            self._redis = StrictRedis(decode_responses=False, **self._conn_kwargs)
+        return self._redis
+
+    def clear(self):
+        if self._redis:
+            self._redis.delete(self._key)
 
 
 class QueueBackend(QueueInterface):
@@ -19,29 +39,28 @@ class QueueBackend(QueueInterface):
         super(QueueBackend, self).__init__(spider_name, **kwargs)
         self.spider_name = spider_name
         if queue_name is None:
-            queue_name = 'task_queue_%s' % spider_name
+            queue_name = "task_queue_%s" % spider_name
         self.queue_name = queue_name
-        self.queue_object = PriorityQueue(queue_name, **kwargs)
-        logging.debug('Redis queue key: %s', self.queue_name)
+        self.queue_object = CustomPriorityQueue(queue_name, **kwargs)
+        logging.debug("Redis queue key: %s", self.queue_name)
 
     def put(self, task, priority, schedule_time=None):
         if schedule_time is not None:
-            raise SpiderMisuseError('Redis task queue does not support '
-                                    'delayed task')
+            raise SpiderMisuseError("Redis task queue does not support " "delayed task")
         # Add attribute with random value
         # This is required because qr library
         # does not allow to store multiple values with same hash
         # in the PriorityQueue
 
         task.redis_qr_rnd = random.random()
-        self.queue_object.push(task, priority)
+        self.queue_object.push({pickle.dumps(task): priority})
 
     def get(self):
         task = self.queue_object.pop()
         if task is None:
             raise queue.Empty()
         else:
-            return task
+            return pickle.loads(task[0])
 
     def size(self):
         return len(self.queue_object)
