a992c126ae6124db4c694afde7b139153194dfa3
User <guest@example.com>
2025-09-15 16:54:12 +0300

    Include forgotten database test config for github

---
+++ test_settings_github.py
@@ -0,0 +1,26 @@
+MONGODB_CONNECTION = {
+    # key must be "database"
+    "database": "grab_test",
+}
+
+MYSQL_CONNECTION = {
+    # key must be "database"
+    "database": "grab_test",
+    # driver specific keys
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "github",
+    "passwd": "github",
+}
+
+REDIS_CONNECTION = {}
+
+POSTGRESQL_CONNECTION = {
+    # key must be "database"
+    "database": "grab_test",
+    # driver specific keys
+    "host": "127.0.0.1",
+    "port": 5432,
+    "user": "github",
+    "password": "github",
+}
