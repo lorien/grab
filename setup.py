100220eaf0f68c127cf123549c0504fa8c10ec80
User <guest@example.com>
2025-09-15 14:41:16 +0300

    Add urllib3 and pyquery setup extras

--- setup.py
+++ setup.py
@@ -24,6 +24,18 @@ setup(
         'typing-extensions; python_version <= "2.7"',
     ],
     extras_require={
-        "full": ["urllib3", "certifi"],
+        "full": [  # deprecated
+            "urllib3",
+            "certifi",
+        ],
+        "urllib3": [
+            "urllib3",
+            "certifi",
+        ],
+        "pyquery": [
+            'pyquery; platform_system != "Windows" and python_version >= "3.0"',
+            'pyquery; platform_system == "Windows" and python_version >= "3.13"',
+            'pyquery <= 1.4.1; platform_system != "Windows" and python_version <= "2.7"',
+        ],
     },
 )
